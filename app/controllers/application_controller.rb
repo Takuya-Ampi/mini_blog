8d98ccfc98c446eddabe93c60328f89c06d0d016
行比拓也 <imac@iMac.local>
2020-05-04 16:44:53 +0900

    ログイン判定機能実装（ログインしていないユーザーのアクセスを制限）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   before_action :set_current_user
+
   def set_current_user
     #変数にセッションidと一致するユーザー情報を格納する
     @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user
+    #ログインしているユーザーが存在しないとき
+    if @current_user == nil
+      #フラッシュメッセージ
+      flash[:notice] = "ログインが必要です"
+      #リダイレクト
+      redirect_to("/login")
+    end
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :authenticate_user
+
   #投稿一覧ページ
   def index
     @posts = Post.all
