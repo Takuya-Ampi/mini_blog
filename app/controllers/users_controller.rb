ef643a631ad1a8389d0547ef503eaae3fac05de6
行比拓也 <imac@iMac.local>
2020-05-04 16:14:59 +0900

    ユーザー登録時にログイン状態にする機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(name: params[:name], email: params[:email], password: params[:password])
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
     #ユーザー一覧にリダイレクト
       redirect_to("/users/index")
