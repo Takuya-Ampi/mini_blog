63766453f43f1652b4eb55e1d8bf80e82f1f5663
行比拓也 <imac@iMac.local>
2020-05-17 15:26:05 +0900

    マイグレーションでエラー出てたので、db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200501132701) do
+ActiveRecord::Schema.define(version: 20200517061711) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
