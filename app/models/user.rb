9c10be4798638a90ba421d96d411a1ee56a9da44
行比拓也 <imac@iMac.local>
2020-05-20 00:20:28 +0900

    ユーザー一覧ページおよびユーザー詳細ページ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :name, {presence: true, length: {maximum: 20}}
   validates :email, {presence: true, uniqueness: true}
   validates :password, {presence: true, length: { minimum: 6, maximum: 12 }}
+  def posts
+    return Post.where(user_id: self.id)
+  end
 end
