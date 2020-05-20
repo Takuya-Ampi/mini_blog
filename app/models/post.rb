8fc763654c89586f8b8abca2a067e193562cc7e4
行比拓也 <imac@iMac.local>
2020-05-20 11:37:45 +0900

    アソシエーションの設定終了

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
   validates :content, {presence: true, length: {maximum: 400}}
   validates :title, {presence: true, length: {maximum: 20}}
   validates :user_id, {presence: true}
