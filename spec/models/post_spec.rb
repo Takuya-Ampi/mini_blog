d1abf04c462eb001f8f701c48e35fe26fe80b1a6
行比拓也 <imac@iMac.local>
2020-05-24 17:51:17 +0900

    テストコードの記載方法変更

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,33 +2,53 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   # ポストモデルのバリデーションのテスト
-  it "本文、タイトル、ユーザーidがある場合、有効である" do
-    post = FactoryBot.build(:post)
-    expect(post).to be_valid
-  end
-  it "本文がない場合、無効である" do
-    post = FactoryBot.build(:post, content: nil)
-    post.valid?
-    expect(post.errors[:content]).to include("を入力してください")
-  end
-  it "本文の長さが401文字以上の場合、無効である" do
-    post = FactoryBot.build(:post, content:  "a" * 401)
-    post.valid?
-    expect(post.errors[:content]).to include("は400文字以内で入力してください")
-  end
-  it "タイトルがない場合、無効である" do
-    post = FactoryBot.build(:post, title: nil)
-    post.valid?
-    expect(post.errors[:title]).to include("を入力してください")
-  end
-  it "タイトルの長さが21文字以上の場合、無効である" do
-    post = FactoryBot.build(:post, title:  "a" * 21)
-    post.valid?
-    expect(post.errors[:title]).to include("は20文字以内で入力してください")
-  end
-  it "ユーザーidがない場合、無効である" do
-    post = FactoryBot.build(:post, user_id: nil)
-    post.valid?
-    expect(post.errors[:user_id]).to include("を入力してください")
+  describe 'バリデーション' do
+    context '本文、タイトル、ユーザーidがある場合' do
+      it "有効" do
+        post = FactoryBot.build(:post)
+        expect(post).to be_valid
+      end
+    end
+    describe '本文' do
+      context '本文がない場合' do
+        it "無効" do
+          post = FactoryBot.build(:post, content: nil)
+          post.valid?
+          expect(post.errors[:content]).to include("を入力してください")
+        end
+      end
+      context '本文の長さが401文字以上の場合' do
+        it "無効" do
+          post = FactoryBot.build(:post, content:  "a" * 401)
+          post.valid?
+          expect(post.errors[:content]).to include("は400文字以内で入力してください")
+        end
+      end
+    end
+    describe 'タイトル' do
+      context 'タイトルがない場合' do
+        it "無効" do
+          post = FactoryBot.build(:post, title: nil)
+          post.valid?
+          expect(post.errors[:title]).to include("を入力してください")
+        end
+      end
+      context 'タイトルの長さが21文字以上の場合' do
+        it "無効" do
+          post = FactoryBot.build(:post, title:  "a" * 21)
+          post.valid?
+          expect(post.errors[:title]).to include("は20文字以内で入力してください")
+        end
+      end
+    end
+    describe 'ユーザーid' do
+      context 'ユーザーidがない場合' do
+        it "無効" do
+          post = FactoryBot.build(:post, user_id: nil)
+          post.valid?
+          expect(post.errors[:user_id]).to include("を入力してください")
+        end
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,48 +2,72 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # ユーザーモデルのバリデーションのテスト
-  it "名前、メール、パスワードがある場合、有効である" do
-    user = FactoryBot.build(:user)
-    expect(user).to be_valid
-  end
-  it "名前がない場合、無効である" do
-    user = FactoryBot.build(:user, name: nil)
-    # valid?メソッドを呼び出すとエラー検証が行える。次行のerrorsメソッドを使うために必要。
-    user.valid?
-    # valid?メソッドでfalseであれば、user.errosでどんなerrorを持っているか返してくれる。今回は特に[:name]のエラーがみたいのでexpect()内部で指定する。
-    expect(user.errors[:name]).to include("を入力してください")
-  end
-  it "名前の長さが21文字以上の場合、無効である" do
-    user = FactoryBot.build(:user, name:  "a" * 21)
-    user.valid?
-    expect(user.errors[:name]).to include("は20文字以内で入力してください")
-  end
-  it "メールアドレスがない場合、無効である"  do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("を入力してください")
-  end
-  it "重複したメールアドレスの場合、無効である" do
-    # FactoryBot.createとFactoryBot.buildの違いは、テストDBに値を保持するかどうか。createは値を保持して、buildはオブジェクト化するだけ。
-    # emailの重複を確認したいので、DBに値が必要
-    user1 = FactoryBot.create(:user)
-    user2 = FactoryBot.build(:user)
-    user2.valid?
-    expect(user2.errors[:email]).to include("はすでに存在します")
-  end
-  it "パスワードがない場合、無効である" do
-    user = FactoryBot.build(:user, password: nil)
-    user.valid?
-    expect(user.errors[:password]).to include("を入力してください")
-  end
-  it "パスワードが5文字以下の場合、無効である" do
-    user = FactoryBot.build(:user, password: "a" * 5)
-    user.valid?
-    expect(user.errors[:password]).to include("は6文字以上で入力してください")
-  end
-  it "パスワードが13文字以上の場合、無効である" do
-    user = FactoryBot.build(:user, password: "a" * 13)
-    user.valid?
-    expect(user.errors[:password]).to include("は12文字以内で入力してください")
+  describe 'バリデーション' do
+    context '名前、メール、パスワードがある場合' do
+      it "有効" do
+        user = FactoryBot.build(:user)
+        expect(user).to be_valid
+      end
+    end
+    describe '名前' do
+      context '名前がない場合' do
+        it "無効" do
+          user = FactoryBot.build(:user, name: nil)
+          # valid?メソッドを呼び出すとエラー検証が行える。次行のerrorsメソッドを使うために必要。
+          user.valid?
+          # valid?メソッドでfalseであれば、user.errosでどんなerrorを持っているか返してくれる。今回は特に[:name]のエラーがみたいのでexpect()内部で指定する。
+          expect(user.errors[:name]).to include("を入力してください")
+        end
+      end
+      context '名前の長さが21文字以上の場合' do
+        it "無効" do
+          user = FactoryBot.build(:user, name:  "a" * 21)
+          user.valid?
+          expect(user.errors[:name]).to include("は20文字以内で入力してください")
+        end
+      end
+    end
+    describe 'メールアドレス' do
+      context 'メールアドレスがない場合' do
+        it "無効"  do
+          user = FactoryBot.build(:user, email: nil)
+          user.valid?
+          expect(user.errors[:email]).to include("を入力してください")
+        end
+      end
+      context 'メールアドレスが重複している場合' do
+        it "無効" do
+          # FactoryBot.createとFactoryBot.buildの違いは、テストDBに値を保持するかどうか。createは値を保持して、buildはオブジェクト化するだけ。
+          # emailの重複を確認したいので、DBに値が必要
+          user1 = FactoryBot.create(:user)
+          user2 = FactoryBot.build(:user)
+          user2.valid?
+          expect(user2.errors[:email]).to include("はすでに存在します")
+        end
+      end
+    end
+    describe 'パスワード' do
+      context 'パスワードがない場合' do
+        it "無効" do
+          user = FactoryBot.build(:user, password: nil)
+          user.valid?
+          expect(user.errors[:password]).to include("を入力してください")
+        end
+      end
+      context 'パスワードが5文字以下の場合' do
+        it "無効" do
+          user = FactoryBot.build(:user, password: "a" * 5)
+          user.valid?
+          expect(user.errors[:password]).to include("は6文字以上で入力してください")
+        end
+      end
+      context 'パスワードが13文字以上の場合' do
+        it "無効" do
+          user = FactoryBot.build(:user, password: "a" * 13)
+          user.valid?
+          expect(user.errors[:password]).to include("は12文字以内で入力してください")
+        end
+      end
+    end
   end
 end
