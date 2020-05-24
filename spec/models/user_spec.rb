15e6495ca4cad66583c76cb5c8f53262d75e10ab
行比拓也 <imac@iMac.local>
2020-05-24 13:05:57 +0900

    FactoryBotを導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "名前、メール、パスワードがある場合、有効である" do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
 end
